01d5459d1d76df5bb06f2998a5cbe35ffd9dc2b3
Daniel Yowell <danielyowell@ou.edu>
2023-01-27 18:45:11 -0600

    successfully created relative path to txt files

--- state.py
+++ state.py
@@ -3,3 +3,6 @@
 # INPUT: a file with a list of actions played of the form (N, X) where N is a position from 0-8
 # OUTPUT: a file describing the state of the board with 9 pairs of values of the form (N,P) 
 #         where N is the position (0-8) and P is what is placed there (X or __ ) 
+
+file1 = open(".\\txt-files\\actions-input1.txt", "r")
+print(file1.readlines())
