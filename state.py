e1f736abc30371073a7e0d83c14d24b864791cac
Daniel Yowell <danielyowell@ou.edu>
2023-01-27 18:56:09 -0600

    created output files and sorted folder

--- state.py
+++ state.py
@@ -4,5 +4,14 @@
 # OUTPUT: a file describing the state of the board with 9 pairs of values of the form (N,P) 
 #         where N is the position (0-8) and P is what is placed there (X or __ ) 
 
-file1 = open(".\\txt-files\\actions-input1.txt", "r")
+file1 = open("txt-files\input\\actions-input1.txt", "r")
+file1.readline() # skip intro comment
 print(file1.readlines())
+
+file2 = open("txt-files\output\state-output.txt", "w")
+file2.write("hello world")
+file2.close()
+
+file3 = open("txt-files\output\what-actions-output.txt", "w")
+file3.write("hello again")
+file3.close()
